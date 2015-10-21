59cf336f272a7f9603e1fcd2e301d68a96b0957b
sharifmansour <sharif.mansour@ryerson.ca>
2015-10-21 16:29:46 -0400

    Added new cout line.

---
+++ gitpractice/src/gitpractice1.1.cpp
@@ -0,0 +1,16 @@
+//============================================================================
+// Name        : gitpractice.cpp
+// Author      : Sharif Mansour
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	cout << "\nMy name is Sharif Mansour and i like cheese.";
+	return 0;
+}
